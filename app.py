983f888fcb7f53c380834f0ecd07807dcc35d18c
ymzk-jp <masaodx007@gmail.com>
2018-07-03 13:35:19 +0900

    python -> ruby

--- app.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-for i in range(10){
-    print 'hello isaax'
-    sleep(10)
-}
---
+++ app.rb
@@ -0,0 +1,5 @@
+STDOUT.sync = true
+while true do
+  puts "Hello Isaax, I'm Raspberry Pi."
+  sleep(5)
+end
